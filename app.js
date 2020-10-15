c35472c08ff10e848c0d0890a8306056b19fea9c
DESKTOP-NUCAED7\ortiz <ortizalej199@gmail.com>
2020-10-15 20:07:52 -0300

    newSchemas

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express')
 const bodyParser = require("body-parser")
 const mongoose = require('mongoose')
 const http = require('http');
-require('./User')
+require('./dataModel')
 const app = express()
 
 app.use(bodyParser.json())
 
-const userData = mongoose.model('user')
-const mongouri = 'mongodb+srv://ortizalej:24472872@cluster0.asl0p.mongodb.net/test?retryWrites=true&w=majority';
+const userSchema = mongoose.model('user')
+const questionSchema = mongoose.model('question')
+
+const mongouri = 'mongodb+srv://ortizalej:24472872@api.hfxha.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(mongouri, {
     useNewUrlParser: true,
@@ -22,15 +24,61 @@ app.get('/', (req, res) => {
     res.send('success')
 })
 
-app.post('/send', (req, res) => {
 
+app.post('/updateEncuesta', (req, res) => {
+    //Update a la tabla y la encuesta el estado
 })
 
-app.post('/get', (req, res) => {
+app.post('/getEncuesta', (req, res) => {
+    //Buscar por Empresa-Encuesta
 })
+
+app.post('/getTableData', (req, res) => {
+    //Llamar en el render del Home()
+})
+
+app.post('/getUsers', (req, res) => {
+    //Login y get lista de users
+})
+
+app.post('/createUser', (req, res) => {
+    //crear Usuario
+
+    const user = new userSchema({
+        username: req.body.username,
+        password: req.body.password,
+        role: req.body.role
+    })
+    user.save().
+        then(data => {
+            console.log(data)
+            res.send({ status: "success" })
+        }).catch(err => {
+            console.log(err)
+            res.send({ status: "error" })
+        })
+})
+
+app.post('/updateUser', (req, res) => {
+    const user = new userSchema({
+        username: req.body.username,
+        password: req.body.password,
+        role: req.body.role
+    })
+    let query = {'username': req.body.userData.username};
+
+    user.updateOne(query,req.body.newData,{upsert: true}, function(err, doc) {
+        if (err) return res.send(500, {error: err});
+        return res.send('Succesfully saved.');
+    });
+})
+
+app.delete('/deleteUser', (req, res) => {
+    //borrar Usuario
+})
+
 
 app.set('port', process.env.PORT || 3000);
-
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
---
+++ dataModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const optionsSchema = new mongoose.Schema({
+    selected: Boolean,
+    label: String
+})
+
+const answerSchema = new mongoose.Schema({
+    label: String, //Pregunta abierta
+    options: [optionsSchema], //Pregunta multiple choice
+    path: String, //Pregunta descarga
+    comment: [String]
+})
+
+const questionSchema = new mongoose.Schema({
+    question: String,
+    type: Number,
+    answer: [answerSchema]
+})
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    role: String
+})
+
+const tableSchema = new mongoose.Schema({
+    surveyName: String,
+    companyName: String,
+    status: String
+})
+
+const surveySchema = new mongoose.Schema({
+    id: String,
+    questions: [questionSchema]
+})
+
+mongoose.model("user", userSchema)
+mongoose.model("question", questionSchema)
+mongoose.model("survey", surveySchema)
+mongoose.model("table", tableSchema)
