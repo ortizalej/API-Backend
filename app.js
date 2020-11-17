a00327e3e5b145094090a37575e8d27fc57eb3c2
Alejandro <ortizalej199@gmail.com>
2020-11-17 18:34:10 -0300

    backend

--- app.js
+++ app.js
@@ -139,7 +139,7 @@ app.post('/updateUser', (req, res) => {
 
 app.delete('/deleteUser', (req, res) => {
     try {
-        User.deleteOne({
+        User.findOneAndDelete({
             _id: req.body._id
         }).then(data => {
             console.log(data)
